7244edb289620cdf0531e06db63a8a77e91cdee9
danielefongo <danielefongo@gmail.com>
2020-02-07 10:09:01 +0100

    take last timeentry

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 
 API_TOKEN=process.env.TOGGL_TOKEN
+WORKSPACE=process.env.TOGGL_WORKSPACE
 
 var TogglClient = require('toggl-api');
 var toggl = new TogglClient({apiToken: API_TOKEN});
 
-toggl.getUserData({}, function(err, data) {
-  console.log(data)
-  toggl.destroy()
-});
+async function getLastTimeEntry(workspace_id) {
+  return new Promise((resolve, reject) => {
+    toggl.getTimeEntries(function(err, data) {
+      if(err) reject()
+      resolve(data
+        .filter(element => element.wid == workspace_id)
+        .pop()
+      );
+    })
+  })
+}
+
+getLastTimeEntry(WORKSPACE).then(console.log)
