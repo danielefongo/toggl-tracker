1e5b26e63116db710812cfa34c757eaba8db549b
danielefongo <danielefongo@gmail.com>
2020-02-07 14:46:01 +0100

    introduce fuzzy search to use partial name or id as argument

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const TogglClient = require('toggl-api');
 const TimeSlotter = require('./timeSlotter')
+const FuzzySearch = require('fuzzy-search')
 
 API_TOKEN=process.env.TOGGL_TOKEN
 WORKSPACE=process.env.TOGGL_WORKSPACE
@@ -24,25 +25,53 @@ async function getLastTimeEntry(workspace_id) {
   })
 }
 
-function createTimeEntryByCopying(timeEntry, projectId, description) {
-  if(description === undefined && projectId == undefined) {
+async function getProjects(workspace_id) {
+  return new Promise((resolve, reject) => {
+    toggl.getWorkspaceProjects(workspace_id, {}, function(err, data) {
+      if(err) reject()
+      resolve(data)
+    });
+  })
+}
+
+async function searchProject(keyword) {
+  return getProjects(WORKSPACE).then(function(projs) {
+    const searcher = new FuzzySearch(projs, ['name', 'id'], {caseSensitive: false, sort: true});
+    return searcher.search(keyword)
+  })
+}
+
+async function createTimeEntryByCopying(timeEntry, projectIdOrName, description) {
+  if(description === undefined && projectIdOrName == undefined) {
     description = timeEntry.description
-    projectId = timeEntry.pid
+    projectIdOrName = timeEntry.pid
   }
+  projects = await searchProject(projectIdOrName)
+
+  if(projects.length == 0) {
+    console.log("No matching for \"" + projectIdOrName + "\"")
+    return
+  }
+  if(projects.length > 1) {
+    console.log("Too many matching projects:")
+    console.log(projects.map(it => it.name))
+    return
+  }
+  project = projects[0]
 
   var timeEntryStop = new Date(timeEntry.stop)
   timeSlotter.slotsFrom(timeEntryStop).forEach((timeSlot) => {
     toggl.createTimeEntry(
       {
         description: description,
-        pid: projectId,
+        pid: project.id,
         duration: timeSlot.duration,
         start: timeSlot.start,
         created_with: "toggl-sheet"
       }, (err) => {
         if(err) console.log(err)
         else {
-          console.log("recorded \"" + description + "\" from " + timeSlot.start.toISOString() + " to " + timeSlot.end.toISOString())
+          console.log("recorded \"" + description + "\" for \"" + project.name + "\" from " + timeSlot.start.toISOString() + " to " + timeSlot.end.toISOString())
         }
       })
   })
