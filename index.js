20c700a05413453365f82eedd6a5d5b7d201a208
danielefongo <danielefongo@gmail.com>
2020-02-07 08:53:39 +0100

    add toggl-api

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 require('dotenv').config()
 
-console.log("hello " + process.env.FOO)
+API_TOKEN=process.env.TOGGL_TOKEN
+
+var TogglClient = require('toggl-api');
+var toggl = new TogglClient({apiToken: API_TOKEN});
+
+toggl.getUserData({}, function(err, data) {
+  console.log(data)
+  toggl.destroy()
+});
