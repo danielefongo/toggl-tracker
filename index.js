e79e0624bddef15838f7de04ef3b2eaeeb15fe60
danielefongo <danielefongo@gmail.com>
2020-02-09 16:29:51 +0100

    move question methods to separate file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const Toggl = require('./toggl');
 const TimeSlotter = require('./timeSlotter')
-const FuzzySearch = require('fuzzy-search')
+const Asker = require('./asker')
 
 API_TOKEN=process.env.TOGGL_TOKEN
 WORKSPACE=process.env.TOGGL_WORKSPACE
@@ -11,14 +11,7 @@ const intervals = [new UTCHoursInterval(8, 12), new UTCHoursInterval(13, 17)]
 
 var toggl = new Toggl(API_TOKEN);
 var timeSlotter = new TimeSlotter(intervals)
-var inquirer = require('inquirer');
-
-inquirer.registerPrompt('ask-for-project', require('inquirer-autocomplete-prompt'));
-
-async function searchProject(projects, keyword) {
-  const searcher = new FuzzySearch(projects, ['name'], {caseSensitive: false, sort: true});
-  return searcher.search(keyword)
-}
+var asker = new Asker()
 
 async function createTimeEntry(timeEntry, project, description) {
   var timeEntryStop = new Date(timeEntry.stop)
@@ -35,57 +28,22 @@ async function createTimeEntry(timeEntry, project, description) {
   })
 }
 
-async function whatHaveYouDone() {
-  answer = await inquirer.prompt([{
-    name: 'description',
-    message: 'What have you done?',
-  }])
-
-  return answer.description
-}
-
-async function chooseProject(projects) {
-  answer = await inquirer.prompt([{
-    type: 'ask-for-project',
-    name: 'projectName',
-    message: 'Select project name',
-    source: function(previousAnser, id) {
-      return searchProject(projects, id)
-    },
-  }])
-
-  return projects.filter(it => it.name == answer.projectName)[0]
-}
-
-async function shouldContinueLastActivity(projectName, description) {
-  answer = await inquirer.prompt([{
-      type: 'list',
-      name: 'usePreviousEntry',
-      message: 'Continue with the previous activity? ("' + description + '" on project "' + projectName + '")',
-      choices: [
-        'Yes', 'No',
-      ],
-    }
-  ])
-  return answer.usePreviousEntry == "Yes"
-}
-
-async function ask() {
+async function compileToggl() {
   lastTimeEntry = await toggl.getLastTimeEntry(WORKSPACE)
   lastTimeEntryProject = await toggl.getProject(lastTimeEntry.pid)
   lastTimeEntryProjectName = lastTimeEntryProject.name
   lastTimeEntryDescription = lastTimeEntry.description
 
-  continueLastActivity = await shouldContinueLastActivity(lastTimeEntryProjectName, lastTimeEntryDescription)
+  continueLastActivity = await asker.shouldContinueLastActivity(lastTimeEntryProjectName, lastTimeEntryDescription)
 
   if (continueLastActivity == true)
     createTimeEntry(lastTimeEntry, lastTimeEntryProject, lastTimeEntryDescription)
   else {
     projects = await toggl.getProjects(WORKSPACE)
-    description = await whatHaveYouDone()
-    project = await chooseProject(projects)
+    description = await asker.whatHaveYouDone()
+    project = await asker.chooseProject(projects)
     createTimeEntry(lastTimeEntry, project, description)
   }
 }
 
-ask()
+compileToggl()
